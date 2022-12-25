a98f6c26a22029eaa86b12d0e06837d358b12557
Jose Melo <jmelo@live.cl>
2022-12-25 18:17:40 -0300

    added yield model

---
+++ src/models/staticyieldmodel.cpp
@@ -0,0 +1,60 @@
+#include <atlas/models/staticyieldmodel.hpp>
+
+namespace Atlas {
+
+    StaticYieldModel::StaticYieldModel(const MarketRequest& marketRequest, const QuantLib::InterestRate& yield, const QuantLib::Date& refDate)
+    : Model(marketRequest), yield_(yield), refDate_(refDate) {}
+
+    void StaticYieldModel::simulate(const std::vector<QuantLib::Date>& evalDates, Scenario& scenario) const {
+        for (const auto& evalDate : evalDates) {
+            MarketData marketData;
+            marketData.allocate(marketRequest_);
+
+            simulateDiscounts(evalDate, marketData);
+            simulateForwards(evalDate, marketData);
+            marketData.refDate = evalDate;
+            scenario.push_back(marketData);
+        }
+    }
+
+    void StaticYieldModel::simulateDiscounts(const QuantLib::Date& refDate, MarketData& md) const {
+        for (auto& request : marketRequest_.dfs) {
+            const auto& date = request.date_;
+            double df;
+            if (refDate < date) {
+                df = yield_.discountFactor(refDate_, date);
+            } else if (refDate == date) {
+                df = 1;
+            } else {
+                df = 0;
+            }
+            md.dfs.push_back(df);
+        }
+    }
+
+    void StaticYieldModel::simulateForwards(const QuantLib::Date& refDate, MarketData& md) const {
+        for (auto& request : marketRequest_.fwds) {
+            const auto& startDate = request.startDate_;
+            const auto& endDate   = request.endDate_;
+            double fwd;
+
+            if (endDate < refDate) {
+                fwd = 0;
+            } else {
+                fwd = yield_
+                          .impliedRate(yield_.compoundFactor(startDate, endDate), request.dayCounter_, request.compounding_, request.frequency_,
+                                       startDate, endDate)
+                          .rate();
+            }
+            md.fwds.push_back(fwd);
+        }
+    }
+
+    MarketData StaticYieldModel::simulate() const {
+        std::vector<QuantLib::Date> dates = {refDate_};
+        Scenario scenario;
+        simulate(dates, scenario);
+        return scenario.front();
+    }
+
+}  // namespace Atlas
